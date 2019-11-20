a846a4a9b4a773d4815fed1be16aa42eb676696f
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-11-20 15:41:50 -0600

    post-crash

--- wrangle_fitbit.py
+++ wrangle_fitbit.py
@@ -1,5 +1,6 @@
 from debug import local_settings, timeifdebug, timeargsifdebug, frame_splain
 import pandas as pd
+import numpy as np
 from datetime import timedelta, datetime
 from acquire import check_df
 import csv
@@ -26,13 +27,18 @@ def acquire_fitbit_daily(directory='fitbit', splain=False):
     df.Date = pd.to_datetime(df.Date, format='%Y-%m-%d')
     df = df.sort_values(by='Date')
     df = df.rename(columns={'Date': 'date'})
-    df = df.set_index('date')
     
     return check_df(df, splain=splain)
 
 
 def get_activities_data(directory='fitbit', splain=local_settings.splain):
     df = acquire_fitbit_daily(directory=directory)
+    df = prep_activities_data(df, splain=splain)
+
+    return check_df(df, splain=splain)
+
+
+def prep_activities_data(df, splain=local_settings.splain):
     df = df.rename(columns={
         'Date': 'date',
         'Calories Burned': 'cals_burned',
@@ -46,6 +52,7 @@ def get_activities_data(directory='fitbit', splain=local_settings.splain):
         'Activity Calories': 'activity_cals',
     })
     #df.date=pd.to_datetime(df.date, format='%Y-%m-%d')
+    df = df.set_index('date')
     for col in df.columns[df.dtypes == 'object']:
         df[col] = df[col].str.replace(',','').astype('float')
     df['mins_tot'] = df.mins_sed + df.mins_light + df.mins_mod + df.mins_heavy
@@ -59,22 +66,25 @@ def get_activities_data(directory='fitbit', splain=local_settings.splain):
     return check_df(df, splain=splain)
 
 
-def predict_missing(df):
+def predict_missing(df, to_csv=True, csv_name=r'missing_data_predicted.csv'):
+    new_df = df[df.index < '2018-07-07'].drop(columns='week_day')
     df_early = df[(df.steps > 0) & (df.index < '2018-07-07')]
-    df_missing = df[df.steps == 0]
+    df_missing = df[(df.steps == 0) & (df.index < '2018-07-07')]
     df_early.drop(columns='week_day',inplace=True)
     df_missing.drop(columns='week_day',inplace=True)
 
     for col in df_missing.columns:
-        df_missing[col]= round(df_early[col].mean())
+        df_missing[col]= round(df_early[col].mean(),2)
 
-    export_csv = df_missing.to_csv(r'missing_data_predicted.csv',header=True)
+    if to_csv: 
+        export_csv = df_missing.to_csv(csv_name,header=True)
     return df_missing
     
     
 def merge_imputed(df, df_mask, imputed_df, agg_fn='mean'):
-    for col in imputed_df.cols:
-        df[col] = np.where(df_mask, df_missing[col].agg(agg_fn), df[col])
+    new_df = df.copy()
+    for col in imputed_df.columns:
+        df[col] = np.where(df_mask, imputed_df[col].agg(agg_fn), df[col])
     return df
  
 
